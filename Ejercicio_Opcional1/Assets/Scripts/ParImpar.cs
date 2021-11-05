455a2da8f0791b4385299e93939c79afe9225cc0
Layton4 <Javierete4774@gmail.com>
2021-11-05 10:28:01 +0100

    privada

--- Ejercicio_Opcional1/Assets/Scripts/ParImpar.cs
+++ Ejercicio_Opcional1/Assets/Scripts/ParImpar.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ParImpar: MonoBehaviour
 {
-    public int num = 0;
+    private int num = 0;
 
     // Start is called before the first frame update
     void Start()
